41359f3f936972bc24e6b7b19deb062058c8aa42
Romano Schoonheim <romano@consumind.nl>
2022-11-30 19:57:12 +0100

    Rector..

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::DEAD_CODE,
+        \Rector\Laravel\Set\LaravelSetList::LARAVEL_CODE_QUALITY,
+    ]);
+
+
+};
