fe4c832b857e2bf43255cd33c063883f64603ff6
Romano Schoonheim <romano@consumind.nl>
2022-11-30 18:42:12 +0100

    Initial commit.

---
+++ src/LaravelApiIntegrationServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Rschoonheim\LaravelApiIntegrationBoilerplate;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LaravelApiIntegrationServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package->name('laravel-api-integration-boilerplate');
+    }
+}
