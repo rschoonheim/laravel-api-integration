7b52457c650bca9bf26fe359e17e28448a942987
Romano Schoonheim <romano@consumind.nl>
2022-11-30 19:48:02 +0100

    Refecturing..

--- src/LaravelApiIntegrationServiceProvider.php
+++ src/LaravelApiIntegrationServiceProvider.php
@@ -11,6 +11,8 @@ class LaravelApiIntegrationServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
+
+
         $package->name('laravel-api-integration-boilerplate');
     }
 }
